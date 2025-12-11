0c26f1f28b9cca4bf5319a24bb31abf8d806fd7b
Catherinegrod <catherine9835@yandex.ru>
2025-12-11 14:16:50 +0300

    init

---
+++ calculator/src/Main.java
@@ -0,0 +1,128 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class Main extends JFrame {
+    private JTextField f;
+    private String c = "";
+    private boolean NiN = true;
+
+    public Main() {
+        super("Calculator");
+        setSize(420, 420);
+        setLayout(null);
+        JTextField f = new JTextField("20");
+        f.setBounds(15, 15, 370, 35);
+        add(f);
+        JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, bP, bMi, bMu, bD, bR;
+        b0 = new JButton("0");
+        b0.setBounds(10, 310, 150, 70);
+        b0.addActionListener((ActionListener) this);
+        add(b0);
+        b1 = new JButton("1");
+        b1.setBounds(10, 230, 70, 70);
+        b1.addActionListener(this);
+        add(b1);
+        b2 = new JButton("2");
+        b2.setBounds(90, 230, 70, 70);
+        b2.addActionListener(this);
+        add(b2);
+        b3 = new JButton("3");
+        b3.setBounds(170, 230, 70, 70);
+        b3.addActionListener(this);
+        add(b3);
+
+        b4 = new JButton("4");
+        b4.setBounds(10, 150, 70, 70);
+        b4.addActionListener(this);
+        add(b4);
+        b5 = new JButton("5");
+        b5.setBounds(90, 150, 70, 70);
+        b5.addActionListener(this);
+        add(b5);
+        b6 = new JButton("6");
+        b6.setBounds(170, 150, 70, 70);
+        b6.addActionListener(this);
+        add(b6);
+        b7 = new JButton("7");
+        b7.setBounds(10, 70, 70, 70);
+        b7.addActionListener(this);
+        add(b7);
+        b8 = new JButton("8");
+        b8.setBounds(10, 70, 70, 70);
+        b8.addActionListener(this);
+        add(b8);
+        b9 = new JButton("9");
+        b9.setBounds(170, 70, 70, 70);
+        b9.addActionListener(this);
+        add(b9);
+
+        bP = new JButton("+");
+        bP.setBounds(250, 70, 70, 70);
+        bP.addActionListener(this);
+        add(bP);
+        bMi = new JButton("-");
+        bMi.setBounds(250, 150, 70, 70);
+        bMi.addActionListener(this);
+        add(bMi);
+        bMu = new JButton("*");
+        bMu.setBounds(250, 230, 70, 70);
+        bMu.addActionListener(this);
+        add(bMu);
+        bD = new JButton(":");
+        bD.setBounds(250, 310, 70, 70);
+        bD.addActionListener(this);
+        add(bD);
+        bR = new JButton("=");
+        bR.setBounds(330, , 70, 70);
+        bR.addActionListener(this);
+        add(bR);
+
+
+    }
+
+    void handleButtonClick(String l) {
+        if ("+-*/".contains(l)) {
+            applyOperator(l);
+        } else if ("=".equals(l)) {
+            calculateResult();
+        } else if ("0123456789".contains(l)) {
+            appendDigit(l);
+        }
+    }
+
+    void appendDigit(String e) {
+        if (NiN) {
+            f.setText(e);
+            NiN = false;
+        } else {
+            String c = f.getText();
+            if (c.equals("0")) {
+                f.setText(e);
+            } else {
+                f.setText(c + e);
+
+            }
+        }
+    }
+
+    private void applyOperator(String p) {
+        if (!c.equals("")) {
+            return;
+        }
+        c = p + " " + f.getText();
+        NiN = true;
+    }
+
+    private void result() {
+
+    }
+
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.setVisible(true);
+    }
+}
+
