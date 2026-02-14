69ace95cb4096240c3cbd2058ed85f49b324aaa5
Catherinegrod <catherine9835@yandex.ru>
2026-02-14 15:03:53 +0300

    Add files via upload

---
+++ f1.js
@@ -0,0 +1,26 @@
+
+let k=0;
+let l=0;
+function h(){
+    
+
+if(k%2==0){
+    document.getElementById("ep1").innerHTML="ЗЛАТАЯ";
+    document.getElementById("ep2").innerHTML="ПРЕЛЕСТНЫЙ"
+
+} else { document.getElementById("ep1").innerHTML="Златая";
+    document.getElementById("ep2").innerHTML="прелестный"
+}
+    k++;
+
+}
+
+function g() {
+    if (l%2==0){
+    document.getElementById("me").innerHTML="ЗВЕЗДОЮ СЕВЕРА ЯВИСЬ"} else {
+        document.getElementById("me").innerHTML="Звездою севера явись";
+    }
+    l++;
+
+}
+
