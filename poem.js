4c84bfb4bedda8a0216fe336ab52e60865ac7a9f
Catherinegrod <catherine9835@yandex.ru>
2026-02-28 12:00:02 +0300

    Update poem.js

--- poem.js
+++ poem.js
@@ -36,6 +36,32 @@ let stihi = [
 <br>значит — это кому-нибудь нужно?
 <br>Значит — кто-то хочет, чтобы они были?
 <br>Значит — кто-то называет эти плево́чки жемчужиной?`
+  },
+  {
+    id:5,
+    nazvanie:"Весенняя гроза",
+    avtor:"Ф.И. Тютчев",
+    janr:"Лирика",
+    epoha:"Романтизм",
+    tekst:`Люблю грозу в начале мая,
+    <br>Когда <epithet>весенний,первый</epithet> <dopepithet>гром</dopepithet>,
+    <br> Как бы <olistetvorenie>резвяся<olistetvorenie> и <olistetvorenie>играя</olistetvorenie>,
+    <br> Грохочет в <dopepithet>небе</dopepithet> <epithet>голубом</epithet>.
+    <br>
+    <br> Гремят <dopepithet>раскаты</dopepithet> <epithet>молодые</epithet>,
+    <br>Вот <metaphor>дождик брызнул</metaphor>, пыль летит,
+    <br>Повисли <dopepithet>перлы</dopepithet> <epithet>дождевые</epithet>,
+    <br>И <metaphor>солнце нити золотит</metaphor>.
+    <br>
+    <br>С горы <olistetvorenie>бежит поток </olistetvorenie>проворный,
+    <br>В лесу не молкнет <epithet>птичий</epithet> <dopepithet>гам</dopepithet>,
+    <br>И гам лесной, и шум нагорный --
+    <br>Все вторит весело громам.
+    <br>
+    <br>Ты скажешь: ветреная Геба,
+    <br>Кормя зевесова орла,
+    <br><epithet>Громокипящий</epithet> <dopepithet>клубок</dopepithet> неба,
+    <br>Смеясь, на землю пролила.`
   }
 ];
 
@@ -64,8 +90,18 @@ if (naidennyi_stih) {
 function vyydelit_epitet() {
   let tekst = document.getElementById("tekst_stiha").innerHTML;
   let novyi_tekst = tekst.replace(/<epithet>(.*?)<\/epithet>/g, '<span class="vydelit_epitet">$1</span>');
+   novyi_tekst = novyi_tekst.replace(/<dopepithet>(.*?)<\/dopepithet>/g, '<span class="doopolnenie_epitet">$1</span>');
   document.getElementById("tekst_stiha").innerHTML = novyi_tekst;
 }
+
+
+
+function vyydelit_olistetvorenie() {
+  let tekst = document.getElementById("tekst_stiha").innerHTML;
+  let novyi_tekst = tekst.replace(/<olistetvorenie>(.*?)<\/olistetvorenie>/g, '<span class="vydelit_olistetvorenie">$1</span>');
+  document.getElementById("tekst_stiha").innerHTML = novyi_tekst;
+}
+
 
 function vyydelit_metaphoru() {
   let tekst = document.getElementById("tekst_stiha").innerHTML;
